287908b884a4893de67607cc3e50d1943e932832
Karthik M M <109535533+parikari33@users.noreply.github.com>
2024-02-25 17:19:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const listContainer=document.getElementById("list-container");
+const inputBox=document.getElementById("input-box");
+
+
+function addTask(){
+    if(inputBox.value ===''){
+        alert("Enter your Task");
+    }else{
+        let li =document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listContainer.appendChild(li);
+
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+
+    inputBox.value="";
+    savetask();
+}
+
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        savetask();
+    }else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        savetask();
+    }
+})
+
+inputBox.addEventListener("keydown",function(e){
+    if(e.key==="Enter"){
+        addTask();
+    }
+})
+
+function savetask(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML=localStorage.getItem("data");
+}
+
+showTask();
